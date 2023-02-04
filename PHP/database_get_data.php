c78c20959cf87afd1abc2cc415ad1b7c1075904d
akarsh <akarshagri121@gmail.com>
2023-02-05 01:53:20 +0530

    aks

--- PHP/database_get_data.php
+++ PHP/database_get_data.php
@@ -55,3 +55,22 @@ function get_stock_hold($stock_name,$email)
         return 0;
     }
 }
+function get_balance_values($email)
+{
+    $user_object = new stdClass();
+    $conn = openCon();
+    $sql = "SELECT * FROM students WHERE (email='$email' || ph_no='$email')";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $arr = array(
+                "username" => $row["username"], "f_name" =>   $row["f_name"], "l_name" => $row["l_name"], "email" => $row["email"], "ph_no" => $row["ph_no"],
+                "wallet_balance" => $row["wallet_balance"],  "user_state" => $row["user_state"]
+            );
+            $json = json_encode($arr);
+            return $json;
+        }
+    } else {
+        return "nf";
+    }
+}
