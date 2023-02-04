5312ab30795219870ed1984234f2baf50f486921
akarsh <akarshagri121@gmail.com>
2023-02-05 01:26:28 +0530

    aks

--- JS/place_data.js
+++ JS/place_data.js
@@ -1,4 +1,4 @@
-let data = JSON.parse(window.localStorage.getItem('result'));
+let data = JSON.parse(window.localStorage.getItem('user_data'));
 console.log(data);
 document.getElementById("user_name_topbar").innerHTML = data.f_name;
 document.getElementById("user_funds").innerHTML = data.wallet_balance;
