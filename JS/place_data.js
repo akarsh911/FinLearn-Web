e8f2f6d3816f5077c20d864b182db2b59c286e3b
akarsh <akarshagri121@gmail.com>
2023-02-05 01:18:20 +0530

    aks

--- JS/place_data.js
+++ JS/place_data.js
@@ -1,2 +1,3 @@
-document.getElementById("user_name_topbar").innerHTML = "New text!";
+let data= JSON.parse(window.localStorage.getItem('result'));
+document.getElementById("user_name_topbar").innerHTML = data.;
 document.getElementById("user_funds").innerHTML = "New text!";
